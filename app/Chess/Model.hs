017a4e4c31dafab984de4681e7563d4b87bfdcb1
fanshi <jackychany321@gmail.com>
2023-07-20 13:21:20 +0800

    refactor and add comments

--- app/Chess/Model.hs
+++ app/Chess/Model.hs
@@ -11,7 +11,6 @@ import Chess.Promotion (pawnPromotionTargets)
 import Control.Applicative ((<|>))
 import Control.Lens (makeLenses, (%~), (.~), (^.))
 import Data.Function ((&))
-import Data.Functor ((<&>))
 import Data.Functor.Base (ListF (..))
 import Data.Functor.Foldable (Recursive (cata))
 import Data.List.NonEmpty as NE (NonEmpty, filter, toList)
@@ -35,84 +34,83 @@ data ChessModel = ChessModel
 makeLenses 'ChessModel
 
 data ChessEvent
-  = ClickedSquare Position
+  = -- NOTE: UI-wise
+    ClickedSquare Position
   | ChessPieceDragDrop (Position, ChessPiece) Position
-  | ChoseChessPiece (Position, ChessPiece)
-  | UnchoseChessPiece (Position, ChessPiece)
-  | ChessPieceMove (Position, ChessPiece) Position ChessBoard
-  | CastlingMove Castling ChessBoard
-  | PawnPromotion Position ChessPieceType
   | ChessBoardResize Rect
+  | -- NOTE: gameplay-wise
+    ChessPieceMove (Either Castling ((Position, ChessPiece), Position)) ChessBoard
+  | PawnPromotion Position ChessPieceType
   | EndTurn
   deriving (Eq, Show)
 
 buildChessUI :: WidgetEnv ChessModel ChessEvent -> ChessModel -> WidgetNode ChessModel ChessEvent
-buildChessUI _wenv (ChessModel eCurrentPlayer _chessBoard _pickedChessPiece _promotingPawnPosition _enPassantColumn _chessBoardScale) =
-  let buidldChessPieceUI chessPiece = label (chessPieceToEmoji chessPiece) `styleBasic` [textColor black, textSize $ 65 * _chessBoardScale]
-   in vstack
-        [ hgrid $ do
-            x@(fromEnum -> x') <- [minBound .. maxBound :: Column]
-            pure . vgrid $ do
-              y@(fromEnum -> y') <- [maxBound, pred maxBound .. minBound :: Row]
-              let pos = Position x y
-                  mChessPiece = lookupChessBoard pos _chessBoard
-                  color = a .~ 0.8 $ case (x' + y') `mod` 2 of
-                    0 -> green
-                    _ -> beige
-                  buidldChessPieceDraggableUI chessPiece = draggable (pos, chessPiece) $ buidldChessPieceUI chessPiece
-              pure . dropTarget (flip ChessPieceDragDrop pos) $
-                box_
-                  (($ ClickedSquare pos) <$> [onClickEmpty, onClick])
-                  (widgetMaybe mChessPiece buidldChessPieceDraggableUI)
-                  `nodeKey` (pack $ show pos)
-                  `styleBasic` [ bgColor $ case mChessPiece of
-                                   Just (ChessPiece owner (King _)) | (_ : _) <- findPosThreats pos _chessBoard owner -> red
-                                   _ ->
-                                     let mix a' b = (a' + 3 * b) `div` 4
-                                         sumColor (Color r g b _) (Color r' g' b' _) = Color (mix r r') (mix g g') (mix b b') 0.8
-                                         colorAlg = \case
-                                           Nil -> Nothing
-                                           Cons (pos', (mPiece, _)) mColor
-                                             | pos' == pos ->
-                                                 Just $
-                                                   color `sumColor` case mPiece of
-                                                     Nothing -> cyan
-                                                     Just _ -> orange
-                                             | otherwise -> mColor
-                                      in fromMaybe color $ _pickedChessPiece >>= \(_, validMoves') -> cata colorAlg validMoves',
-                                 border (5 * _chessBoardScale) $ case _pickedChessPiece of
-                                   Just (((pos ==) -> True, _), _) -> pink
-                                   _ -> transparent
-                               ],
-          case eCurrentPlayer of
-            Right (player, _) ->
-              popupD_
-                (WidgetValue (isJust _promotingPawnPosition))
-                [popupDisableClose, popupOpenAtCursor]
-                $ hstack
-                  ( pawnPromotionTargets <&> \target -> case _promotingPawnPosition of
-                      Just pos -> box_ (($ PawnPromotion pos target) <$> [onClick, onClickEmpty]) . buidldChessPieceUI $ ChessPiece player target
-                      Nothing -> spacer `nodeVisible` False
-                  )
-                  `styleBasic` [bgColor brown]
-            Left result ->
-              popupD_ (WidgetValue True) [popupDisableClose, alignMiddle, alignCenter, popupAlignToWindow] $
-                ( label . pack $ case result of
-                    Left r -> show r
-                    Right r -> show r
-                )
-                  `styleBasic` [bgColor brown]
-        ]
+buildChessUI _wenv (ChessModel eCurrentPlayer _chessBoard _pickedChessPiece _promotingPawnPosition _enPassantColumn _chessBoardScale) = vstack [chessBoardUI, popups]
+  where
+    buidldChessPieceUI chessPiece = label (chessPieceToEmoji chessPiece) `styleBasic` [textColor black, textSize $ 65 * _chessBoardScale]
+    chessBoardUI = hgrid $ do
+      x@(fromEnum -> x') <- [minBound .. maxBound :: Column]
+      pure . vgrid $ do
+        y@(fromEnum -> y') <- [maxBound, pred maxBound .. minBound :: Row]
+        let pos = Position x y
+            mChessPiece = lookupChessBoard pos _chessBoard
+            -- NOTE: basic chessbaord square color
+            color = a .~ 0.8 $ case (x' + y') `mod` 2 of
+              0 -> green
+              _ -> beige
+            buidldChessPieceDraggableUI chessPiece = draggable (pos, chessPiece) $ buidldChessPieceUI chessPiece
+        pure . dropTarget (flip ChessPieceDragDrop pos) $
+          box_
+            ([onClickEmpty, onClick] <*> [ClickedSquare pos])
+            (widgetMaybe mChessPiece buidldChessPieceDraggableUI)
+            `styleBasic` [ bgColor $ case mChessPiece of
+                             -- NOTE: checked King on the square
+                             Just (ChessPiece owner (King _)) | (_ : _) <- findPosThreats pos _chessBoard owner -> red
+                             _ ->
+                               let mix a' b = (a' + 3 * b) `div` 4
+                                   sumColor (Color r g b _) (Color r' g' b' _) = Color (mix r r') (mix g g') (mix b b') 0.8
+                                   colorAlg = \case
+                                     Nil -> Nothing
+                                     Cons (pos', (mPiece, _)) mColor
+                                       -- NOTE: valid potential move
+                                       | pos' == pos ->
+                                           Just $
+                                             color `sumColor` case mPiece of
+                                               Nothing -> cyan
+                                               -- NOTE: when threatening other piece
+                                               Just _ -> orange
+                                       | otherwise -> mColor
+                                in fromMaybe color $ _pickedChessPiece >>= \(_, validMoves') -> cata colorAlg validMoves',
+                           -- NOTE: show border depends on if it is chosen
+                           border (5 * _chessBoardScale) $ case _pickedChessPiece of
+                             Just (((pos ==) -> True, _), _) -> pink
+                             _ -> transparent
+                         ]
+    popups = case eCurrentPlayer of
+      -- NOTE: for choosing what pawn promotiing to
+      Right (player, _) ->
+        popupD_ (WidgetValue (isJust _promotingPawnPosition)) [popupDisableClose, popupOpenAtCursor]
+          . widgetMaybe _promotingPawnPosition
+          $ \pos ->
+            hstack
+              ( do
+                  target <- pawnPromotionTargets
+                  pure . box_ ([onClick, onClickEmpty] <*> [PawnPromotion pos target]) . buidldChessPieceUI $ ChessPiece player target
+              )
+              `styleBasic` [bgColor brown]
+      -- NOTE: for displaying end game result
+      Left result ->
+        popupD_ (WidgetValue True) [popupDisableClose, alignMiddle, alignCenter, popupAlignToWindow] $
+          (label . pack $ either show show result)
+            `styleBasic` [bgColor brown]
 
 handleChessEvent :: WidgetEnv ChessModel ChessEvent -> WidgetNode ChessModel ChessEvent -> ChessModel -> ChessEvent -> [EventResponse ChessModel ChessEvent sp ep]
-handleChessEvent _wenv _node (ChessModel (Left _) _ _ _ _ _) = const []
+handleChessEvent _wenv _node (ChessModel (Left _) _ _ _ _ _) = const [] -- NOTE: Game ended.
 handleChessEvent _wenv _node model@(ChessModel (Right (currentPlayer, validMoves)) _chessBoard _pickedChessPiece _promotingPawnPosition _enPassantColumn _) = \case
   ClickedSquare pos -> case _pickedChessPiece of
-    Nothing -> case lookupChessBoard pos _chessBoard of
-      Just chessPiece@(ChessPiece ((== currentPlayer) -> True) _) -> pure . Event $ ChoseChessPiece (pos, chessPiece)
-      _ -> []
     Just (picked@(pickedPos, _), validMoves')
-      | pickedPos == pos -> pure . Event $ UnchoseChessPiece picked
+      -- NOTE: clicked on the chosen chess piece, unchoose it.
+      | pickedPos == pos -> unchoseChessPiece
       | otherwise ->
           let validMoveChessBoardAlg = \case
                 Nil -> Nothing
@@ -120,54 +118,64 @@ handleChessEvent _wenv _node model@(ChessModel (Right (currentPlayer, validMoves
                   | validPos == pos -> Just updatedChessBoard
                   | otherwise -> r
            in case cata validMoveChessBoardAlg validMoves' of
-                Nothing -> pure $
-                  case lookupChessBoard pos _chessBoard of
-                    Just chessPiece@(ChessPiece ((== currentPlayer) -> True) _) ->
-                      Model $ model & pickedChessPiece .~ Just ((pos, chessPiece), maybe [] NE.toList $ validMoves !? (pos, chessPiece))
-                    _ -> Event $ UnchoseChessPiece picked
-                Just updatedChessBoard -> pure . Event $ ChessPieceMove picked pos updatedChessBoard
+                -- NOTE: no valid move to the clicked square for the chosen piece
+                Nothing -> case lookupChessBoard pos _chessBoard of
+                  Just chessPiece@(ChessPiece ((== currentPlayer) -> True) _) -> pickTheChessPiece chessPiece
+                  _ -> unchoseChessPiece
+                -- NOTE: valid move!
+                Just updatedChessBoard -> pure . Event $ ChessPieceMove (Right (picked, pos)) updatedChessBoard
+    Nothing -> case lookupChessBoard pos _chessBoard of -- NOTE: no chess piece is chosen yet.
+      Just chessPiece@(ChessPiece ((== currentPlayer) -> True) _) -> pickTheChessPiece chessPiece
+      _ -> []
+    where
+      pickTheChessPiece chessPiece@(ChessPiece _ chessPieceType) =
+        let moves = maybe [] NE.toList (validMoves !? (pos, chessPiece))
+            castlingMoves = case chessPieceType of
+              King Unmoved -> do
+                (_, pos', updatedChessBoard) <- generateCastlingPositions _chessBoard currentPlayer
+                pure (pos', (Nothing, updatedChessBoard))
+              _ -> []
+         in [Model $ model & pickedChessPiece .~ Just ((pos, chessPiece), castlingMoves <> moves)]
+      unchoseChessPiece = [Model $ model & pickedChessPiece .~ Nothing]
   ChessPieceDragDrop dragFrom@(_, chessPiece@(ChessPiece _ chessPieceType)) dropTo -> case chessPiece of
     ChessPiece ((== currentPlayer) -> True) _ ->
-      let moves =
-            validMoves !? dragFrom >>= \moves' ->
-              case NE.filter ((== dropTo) . fst) moves' of
-                [] -> Nothing
-                (_, (_, updatedChessBoard)) : _ -> pure . Event $ ChessPieceMove dragFrom dropTo updatedChessBoard
+      let mMatchingValidMoves =
+            NE.filter ((== dropTo) . fst) <$> validMoves !? dragFrom >>= \case
+              [] -> Nothing
+              -- NOTE: valid move matched!
+              (_, (_, updatedChessBoard)) : _ -> pure . Event $ ChessPieceMove (Right (dragFrom, dropTo)) updatedChessBoard
           alg = \case
             Nil -> Nothing
             Cons (castling, pos', updatedChessBoard) r
-              | pos' == dropTo -> pure . Event $ CastlingMove castling updatedChessBoard
+              -- NOTE: valid castling move matched!
+              | pos' == dropTo -> pure . Event $ ChessPieceMove (Left castling) updatedChessBoard
               | otherwise -> r
-          castlingMoves = case chessPieceType of
+          mCastlingMoves = case chessPieceType of
             King Unmoved -> cata alg $ generateCastlingPositions _chessBoard currentPlayer
             _ -> Nothing
-       in maybeToList $ moves <|> castlingMoves
+       in maybeToList $ mMatchingValidMoves <|> mCastlingMoves
+    -- NOTE: can't dragdrop other player's chess piece!
     _ -> []
-  ChessPieceMove (_, (ChessPiece _ chessPieceType)) moveToPos@(Position x y) updatedChessBoard ->
+  ChessPieceMove move updatedChessBoard ->
     let model' =
           model
             & chessBoard .~ updatedChessBoard
             & pickedChessPiece .~ Nothing
             & enPassantColumn .~ Nothing
-        simpleCase = [Model model', Event EndTurn]
-     in case chessPieceType of
-          Pawn Moved ->
-            if (currentPlayer == Player1 && y == R8) || (currentPlayer == Player2 && y == R1)
-              then [Model $ model' & promotingPawnPosition .~ Just moveToPos]
-              else simpleCase
-          Pawn Unmoved ->
-            if (currentPlayer == Player1 && y == R4) || (currentPlayer == Player2 && y == R5)
-              then [Model $ model' & enPassantColumn .~ Just x, Event EndTurn]
-              else simpleCase
-          _ -> simpleCase
-  ChoseChessPiece chessPieceOnBoard@(_, (ChessPiece owner chessPieceType)) ->
-    let helper (_, pos, updatedChessBoard) = (pos, (Nothing, updatedChessBoard))
-        specialMoves = case chessPieceType of
-          King Unmoved -> helper <$> generateCastlingPositions _chessBoard currentPlayer
-          _ -> []
-     in pure . responseIf (owner == currentPlayer) . Model $
-          model & pickedChessPiece .~ Just (chessPieceOnBoard, specialMoves <> maybe [] NE.toList (validMoves !? chessPieceOnBoard))
-  UnchoseChessPiece _ -> [Model $ model & pickedChessPiece .~ Nothing]
+        simplyMove = [Model $ model', Event EndTurn] -- NOTE: simply move and end turn.
+     in case move of
+          Left _ -> simplyMove
+          Right ((_, (ChessPiece _ chessPieceType)), moveToPos@(Position x y)) ->
+            let forPromote = [Model $ model' & promotingPawnPosition .~ Just moveToPos]
+                forEnPassant = [Model $ model' & enPassantColumn .~ Just x, Event EndTurn]
+             in case (chessPieceType, currentPlayer, y) of
+                  -- NOTE: pawn promotion!!!
+                  (Pawn Moved, Player1, R8) -> forPromote
+                  (Pawn Moved, Player2, R1) -> forPromote
+                  -- NOTE: pawn first moved for two squares forward, need to consider en passant for next player(if any).
+                  (Pawn Unmoved, Player1, R4) -> forEnPassant
+                  (Pawn Unmoved, Player2, R5) -> forEnPassant
+                  _ -> simplyMove
   ChessBoardResize rect -> [Model $ model & chessBoardScale .~ min (rect ^. w / 800) (rect ^. h / 600)]
   PawnPromotion pos chessPieceType ->
     [ Model $
@@ -176,18 +184,15 @@ handleChessEvent _wenv _node model@(ChessModel (Right (currentPlayer, validMoves
           & promotingPawnPosition .~ Nothing,
       Event EndTurn
     ]
-  CastlingMove _ updatedChessBoard -> [Model $ model & chessBoard .~ updatedChessBoard, Event EndTurn]
-  EndTurn -> do
+  EndTurn -> pure . Model $ do
     let player' = nextPlayer currentPlayer
         moves = generateValidNonCastlingMoves player' _chessBoard _enPassantColumn
-    pure . Model $
-      if Map.null moves
-        then
-          model
-            & currentPlayerState
-              .~ Left
-                ( case findKingThreats _chessBoard player' of
-                    [] -> Left $ Stalemate
-                    _ : _ -> Right $ Win currentPlayer
-                )
-        else model & currentPlayerState .~ Right (nextPlayer currentPlayer, moves)
+    model
+      & currentPlayerState
+        .~ ( if Map.null moves
+               then -- NOTE: no valid move for the opponent
+               Left $ case findKingThreats _chessBoard player' of
+                 [] -> Left $ Stalemate
+                 _ : _ -> Right $ Win currentPlayer
+               else Right (nextPlayer currentPlayer, moves)
+           )
